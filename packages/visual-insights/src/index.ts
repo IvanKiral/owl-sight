6075b9065acb8e84a5ef0b83cd88947cfc1b94b5
Ivan Kiral <kiralivan@gmail.com>
2026-01-09 19:21:32 +0100

    fix biome for monorepo

--- packages/visual-insights/src/index.ts
+++ packages/visual-insights/src/index.ts
@@ -1,24 +1,25 @@
+// biome-ignore lint/performance/noBarrelFile: barrel file for public API
 export * from "./download.js";
 export { getWebpageData } from "./html/getWebpageData.js";
 export type {
-	WebpageDataResult,
-	WebpageMetadata,
-	WebpageOptions,
-	WebpageResult,
+  WebpageDataResult,
+  WebpageMetadata,
+  WebpageOptions,
+  WebpageResult,
 } from "./html/webpageTypes.js";
 export { getVideoData } from "./utils/videoData.js";
 export * from "./whisper/transcribe.js";
 export {
-	getLanguageName,
-	WHISPER_LANGUAGES,
-	type WhisperLanguage,
-	type WhisperLanguageName,
+  getLanguageName,
+  WHISPER_LANGUAGES,
+  type WhisperLanguage,
+  type WhisperLanguageName,
 } from "./whisper/whisperTypes.js";
 export {
-	type CookieConfig,
-	KEYRINGS,
-	type Keyring,
-	SUPPORTED_BROWSERS,
-	type SupportedBrowser,
+  type CookieConfig,
+  KEYRINGS,
+  type Keyring,
+  SUPPORTED_BROWSERS,
+  type SupportedBrowser,
 } from "./ytdlp/cmdArgs.js";
 export { extractMetadata } from "./ytdlp/metadata.js";
