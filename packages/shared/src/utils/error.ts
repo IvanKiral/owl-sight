449323c6d25c751e2af913c7ed4617499af77e1d
Ivan Kiral <kiralivan@gmail.com>
2026-01-09 12:36:51 +0100

    add utils functions to work with WithError

--- packages/shared/src/utils/error.ts
+++ packages/shared/src/utils/error.ts
@@ -11,3 +11,18 @@ export const error = <E>(error: E): WithError<never, E> => ({
   success: false,
   error,
 });
+
+export const mapResult = <T, U, E>(
+  result: WithError<T, E>,
+  fn: (value: T) => U
+): WithError<U, E> => (result.success ? success(fn(result.result)) : result);
+
+export const flatMapResult = <T, U, E>(
+  result: WithError<T, E>,
+  fn: (value: T) => WithError<U, E>
+): WithError<U, E> => (result.success ? fn(result.result) : result);
+
+export const flatMapResultAsync = async <T, U, E>(
+  result: WithError<T, E>,
+  fn: (value: T) => Promise<WithError<U, E>>
+): Promise<WithError<U, E>> => (result.success ? fn(result.result) : result);
