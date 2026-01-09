77c24fb235e226d5943e96cb2b3e5f205e33cbf3
Ivan Kiral <kiralivan@gmail.com>
2026-01-09 18:00:04 +0100

    fix bug in visual insights of wrong paramter to yrdlp

--- packages/visual-insights/src/download.ts
+++ packages/visual-insights/src/download.ts
@@ -1,37 +1,45 @@
 import * as path from "node:path";
 import { type WithError, error, success } from "shared";
-import { type VideoExtractionArgs, createYtDlpExtractVideoArgs } from "./ytdlp/cmdArgs.js";
+import {
+	type VideoExtractionArgs,
+	createYtDlpExtractVideoArgs,
+} from "./ytdlp/cmdArgs.js";
 import { runYtDlp } from "./ytdlp/runYtdlp.js";
 
 export type YtDlpVideoOptions = Partial<
-  Pick<VideoExtractionArgs, "quality" | "format" | "mergeOutputFormat" | "quiet" | "cookies">
+	Pick<
+		VideoExtractionArgs,
+		"quality" | "format" | "mergeOutputFormat" | "quiet" | "cookies"
+	>
 >;
 
 export const downloadDataFromVideo = async (
-  url: string,
-  dirPath: string,
-  options?: YtDlpVideoOptions & { metadata?: boolean },
-): Promise<WithError<{ videoFilePath: string; metadataFilePath: string }, string>> => {
-  const extension = options?.mergeOutputFormat ?? "mkv";
-  const outTemplate = path.join(dirPath, `video.${extension}`);
+	url: string,
+	dirPath: string,
+	options?: YtDlpVideoOptions & { metadata?: boolean },
+): Promise<
+	WithError<{ videoFilePath: string; metadataFilePath: string }, string>
+> => {
+	const extension = options?.mergeOutputFormat ?? "mkv";
+	const outTemplate = path.join(dirPath, `video.${extension}`);
 
-  const ytdlArgs = createYtDlpExtractVideoArgs({
-    quality: options?.quality,
-    format: options?.format,
-    mergeOutputFormat: options?.mergeOutputFormat,
-    outputPath: outTemplate,
-    writeInfoJson: options?.metadata ?? true,
-    quiet: options?.quiet ?? false,
-    cookies: options?.cookies,
-  });
+	const ytdlArgs = createYtDlpExtractVideoArgs({
+		quality: options?.quality,
+		format: options?.format,
+		mergeOutputFormat: options?.mergeOutputFormat,
+		outputPath: outTemplate,
+		writeInfoJson: options?.metadata ?? true,
+		quiet: options?.quiet ?? false,
+		cookies: options?.cookies,
+	});
 
-  try {
-    await runYtDlp(url, ytdlArgs as string[]);
-    return success({
-      videoFilePath: outTemplate,
-      metadataFilePath: path.join(dirPath, "video.info.json"),
-    });
-  } catch (e) {
-    return error(`Failed to download video: ${e}`);
-  }
+	try {
+		await runYtDlp(url, ytdlArgs as string[]);
+		return success({
+			videoFilePath: outTemplate,
+			metadataFilePath: path.join(dirPath, `video.${extension}.info.json`),
+		});
+	} catch (e) {
+		return error(`Failed to download video: ${e}`);
+	}
 };
